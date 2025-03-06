81a134c171ff5bc1e6443409b27eacd37d8b3fd8
Iamalexxander <yohel.mena@istpet.edu.ec>
2025-03-06 10:58:30 -0500

    Cambiando fotos y estilos

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -9,7 +9,9 @@ import {
     Heading,
 } from "native-base";
 import { Contenedor } from "../../components/contenedor/Contenedor";
-import { ScrollView } from "react-native";
+import { ImageBackground, ScrollView } from "react-native";
+
+const image = require('../../../assets/wanling.png'); 
 
 export const Home = ({ navigation }) => {
     const MenuCard = ({ title, description, route, imageUrl }) => (
@@ -49,44 +51,46 @@ export const Home = ({ navigation }) => {
 
     return (
         <Contenedor>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <VStack space={4} p="4">
-                    <Center>
-                        <Image
-                            size={150}
-                            mt="5"
-                            my="5"
-                            alt="Profile Image"
-                            borderRadius={100}
-                            borderWidth={3}
-                            borderColor="cyan.500"
-                            shadow={5}
-                            source={{
-                                uri: "https://static.wikia.nocookie.net/the-daily-life-of-the-immortal-king/images/d/df/Demon_master.jpg/revision/latest?cb=20220914163338"
-                            }}
-                        />
-                    </Center>
+            <ImageBackground source={image} resizeMode="cover">
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <VStack space={4} p="4">
+                        <Center>
+                            <Image
+                                size={150}
+                                mt="5"
+                                my="5"
+                                alt="Profile Image"
+                                borderRadius={100}
+                                borderWidth={3}
+                                borderColor="cyan.500"
+                                shadow={5}
+                                source={{
+                                    uri: "https://static.wikia.nocookie.net/the-daily-life-of-the-immortal-king/images/a/a4/ShayeNiguth2.jpg/revision/latest?cb=20220915113341"
+                                }}
+                            />
+                        </Center>
 
-                    <Box bg="white" p="5" rounded="xl" shadow={3}>
-                        <Heading textAlign="center" fontSize="2xl">
-                            Prueba web{" "}
-                            <Text color="cyan.500" bold>
-                                Prueba web
+                        <Box bg="white" p="5" rounded="xl" shadow={3}>
+                            <Heading textAlign="center" fontSize="2xl">
+                                Prueba web{" "}
+                                <Text color="cyan.500" bold>
+                                    Prueba web
+                                </Text>
+                            </Heading>
+                            <Text mt="3" fontWeight="medium" textAlign="center" color="gray.600">
+                                Esta prueba fue hecha por Foxy
                             </Text>
-                        </Heading>
-                        <Text mt="3" fontWeight="medium" textAlign="center" color="gray.600">
-                            Esta prueba fue hecha por Foxy
-                        </Text>
-                    </Box>
+                        </Box>
 
-                    <MenuCard
-                        title="Crud de Usuarios"
-                        description="Esta pantalla nos servira para la prueba."
-                        route="Usuarios"
-                        imageUrl="https://lh6.googleusercontent.com/proxy/Iw7S760QwEKoYRokvLPlTxw7c4_ZMgffPJEg6-Vn1x4UBRAnF7QfGFxVawnLkOcfTNTRQ2zLp-XDRvaVj0eUZkSY516hSwN18SXsEkfAZQs"
-                    />
-                </VStack>
-            </ScrollView>
+                        <MenuCard
+                            title="Crud de Usuarios"
+                            description="Esta pantalla nos servira para la prueba."
+                            route="Usuarios"
+                            imageUrl="https://miro.medium.com/v2/resize:fit:512/1*DeDzsGFJu4VkabeY-PVihA.jpeg"
+                        />
+                    </VStack>
+                </ScrollView>
+            </ImageBackground>
         </Contenedor>
     );
 };
